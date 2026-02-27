fad057ef843abc6d38b988e43621bc61c7b4a141
Felipe Fernandes <lipefernandes97@gmail.com>
2026-02-27 04:15:26 -0300

    feat: configuração inicial do projeto com tela de login

---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {}
